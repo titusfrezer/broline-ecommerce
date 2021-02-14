ad16e4fd70c8d2527de41f757e5c722c0411a888
BisratHailu <bisrathailu793@yahoo.com>
2021-02-14 18:15:03 +0300

    HomePage UI

--- lib/Pages/HomePage.dart
+++ lib/Pages/HomePage.dart
@@ -1,33 +1,90 @@
 import 'dart:ffi';
 
+import 'package:broline/Pages/SubCategoryPage.dart';
 import 'package:flutter/material.dart';
 
 import 'package:broline/Models/Colors.dart';
+import 'package:flutter_group_sliver/flutter_group_sliver.dart';
 
 class HomePage extends StatelessWidget {
+  ScrollController scrollController = ScrollController(
+    initialScrollOffset: 10, // or whatever offset you wish
+    keepScrollOffset: true,
+  );
+
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-        child: Scaffold(
-      body: ListView(
-        scrollDirection: Axis.vertical,
-        children: [
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 15),
-            child: Row(
-              children: [
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 5),
-                  child: IconButton(
-                      icon: Icon(
-                        Icons.list,
-                        color: BrolineColor.brolineDarkGrey,
+    return Scaffold(
+        backgroundColor: BrolineColor.brolineWhite,
+        body: NestedScrollView(
+          scrollDirection: Axis.vertical,
+          headerSliverBuilder: (BuildContext context, bool innerBoxScrolled) {
+            return <Widget>[
+              SliverPadding(
+                padding: EdgeInsets.only(top: 20, left: 10, right: 10),
+                sliver: SliverAppBar(
+                  floating: false,
+                  elevation: 0.0,
+                  forceElevated: false,
+                  expandedHeight: 100,
+                  backgroundColor: BrolineColor.brolineWhite,
+                  centerTitle: true,
+                  leading: Container(
+                    height: 60,
+                    padding: EdgeInsets.symmetric(horizontal: 5),
+                    decoration: BoxDecoration(
+                      color: BrolineColor.brolineLightGrey,
+                      shape: BoxShape.circle,
+                    ),
+                    child: IconButton(
+                        icon: Icon(
+                          Icons.list,
+                          color: BrolineColor.brolineDarkGrey,
+                          size: 25,
+                        ),
+                        onPressed: () {}),
+                  ),
+                  actions: [
+                    Container(
+                      height: 60,
+                      padding: EdgeInsets.symmetric(horizontal: 5),
+                      decoration: BoxDecoration(
+                        color: BrolineColor.brolineLightGrey,
+                        shape: BoxShape.circle,
                       ),
-                      onPressed: () {}),
+                      child: IconButton(
+                          icon: Icon(
+                            Icons.shopping_cart,
+                            color: BrolineColor.brolineDarkGrey,
+                            size: 25,
+                          ),
+                          onPressed: () {}),
+                    ),
+                  ],
+                  flexibleSpace: LayoutBuilder(builder:
+                      (BuildContext context, BoxConstraints constraints) {
+                    return FlexibleSpaceBar(
+                      collapseMode: CollapseMode.parallax,
+                      background: Container(
+                        color: Colors.white,
+                        child: Image.asset(
+                          'assets/images/brolineSplashCroped.jpg',
+                          fit: BoxFit.contain,
+                        ),
+                      ),
+                    );
+                  }),
                 ),
-                Expanded(
-                  child: Container(
-                    margin: EdgeInsets.symmetric(horizontal: 5, vertical: 3),
+              ),
+              SliverPadding(
+                padding: EdgeInsets.symmetric(vertical: 20),
+                sliver: SliverAppBar(
+                  floating: true,
+                  pinned: true,
+                  elevation: 0.0,
+                  forceElevated: false,
+                  backgroundColor: BrolineColor.brolineWhite,
+                  title: Container(
                     padding: EdgeInsets.symmetric(horizontal: 15, vertical: 3),
                     decoration: BoxDecoration(
                         color: BrolineColor.brolineLightGrey,
@@ -44,29 +101,112 @@ class HomePage extends StatelessWidget {
                     ),
                   ),
                 ),
-                Container(
-                  height: 60,
-                  padding: EdgeInsets.symmetric(horizontal: 5),
-                  decoration: BoxDecoration(
-                    color: BrolineColor.brolineLightGrey,
-                    shape: BoxShape.circle,
-                  ),
-                  child: IconButton(
-                      icon: Icon(
-                        Icons.shopping_cart,
-                        color: BrolineColor.brolineDarkGrey,
-                        size: 25,
+              ),
+            ];
+          },
+          body: CustomScrollView(
+            slivers: <Widget>[
+              SliverToBoxAdapter(
+                child: Column(
+                  children: [
+                    _buildCategoryList(),
+                    _buildRecentProducts(),
+                    Container(
+                      width: double.maxFinite,
+                      margin: EdgeInsets.only(left: 30, right: 30, top: 50),
+                      child: Text(
+                        "Recent Products",
+                        style: TextStyle(
+                            fontSize: 22, fontWeight: FontWeight.w600),
                       ),
-                      onPressed: () {}),
+                    ),
+                  ],
                 ),
-              ],
-            ),
+              ),
+              // Replace your ListView.builder with this:
+              SliverGroupBuilder(
+                margin: EdgeInsets.only(left: 10, right: 10, top: 25),
+                child: SliverGrid(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      childAspectRatio: 0.75,
+                      crossAxisCount: 2,
+                      crossAxisSpacing: 2,
+                      mainAxisSpacing: 2),
+                  delegate: SliverChildBuilderDelegate(
+                    (BuildContext context, int index) {
+                      return Card(
+                        color: BrolineColor.brolineWhite,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(12)),
+                        ),
+                        child: Container(
+                          padding: EdgeInsets.symmetric(
+                            horizontal: 20,
+                          ),
+                          child: Column(
+                            // crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 10, vertical: 5),
+                                child: Image.asset(
+                                  "assets/images/broline.jfif",
+                                  fit: BoxFit.cover,
+                                  height: 100,
+                                ),
+                              ),
+                              Text(
+                                "Omen Galaxy S10",
+                                style: TextStyle(
+                                    color: BrolineColor.brolineDarkGrey,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 17),
+                                maxLines: 2,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                              ListTile(
+                                contentPadding: EdgeInsets.symmetric(
+                                    horizontal: 0, vertical: 0),
+                                trailing: Container(
+                                  width: 55,
+                                  padding: EdgeInsets.only(
+                                      left: 20, top: 5, bottom: 5),
+                                  decoration: BoxDecoration(
+                                      color: BrolineColor.brolineDarkOrange,
+                                      borderRadius: BorderRadius.only(
+                                          topLeft: Radius.elliptical(50, 35),
+                                          bottomLeft: Radius.circular(100))),
+                                  child: Text(
+                                    "20% off",
+                                    maxLines: 2,
+                                    style: TextStyle(
+                                        color: BrolineColor.brolineWhite),
+                                  ),
+                                ),
+                                title: Text(
+                                  "\$300",
+                                  style: TextStyle(
+                                      color: BrolineColor.brolineDarkOrange,
+                                      fontWeight: FontWeight.w700),
+                                ),
+                                subtitle: Text(
+                                  "\$350",
+                                  style: TextStyle(
+                                      decoration: TextDecoration.lineThrough),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ),
+            ],
           ),
-          _buildCategoryList(),
-          _buildRecentProducts()
-        ],
-      ),
-    ));
+        ));
   }
 
   Widget _buildCategoryList() {
@@ -80,29 +220,36 @@ class HomePage extends StatelessWidget {
               scrollDirection: Axis.horizontal,
               itemCount: 7,
               itemBuilder: (BuildContext context, int index) {
-                return Card(
-                  color: BrolineColor.brolineLightOrange,
-                  shape: RoundedRectangleBorder(
-                    side: BorderSide(
-                        color: BrolineColor.brolineDarkOrange, width: 0.25),
-                    borderRadius: BorderRadius.all(Radius.circular(16)),
-                  ),
-                  child: Container(
-                    width: 80,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        Icon(
-                          Icons.category,
-                          color: BrolineColor.brolineDarkOrange,
-                        ),
-                        Text(
-                          "Category",
-                          style: TextStyle(
-                              color: BrolineColor.brolineDarkOrange,
-                              fontWeight: FontWeight.bold),
-                        )
-                      ],
+                return InkWell(
+                  onTap: (){
+                    Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context){
+                      return SubCategoryPage(categoryName: "SubCategory",);
+                    }));
+                  },
+                  child: Card(
+                    color: BrolineColor.brolineLightOrange,
+                    shape: RoundedRectangleBorder(
+                      side: BorderSide(
+                          color: BrolineColor.brolineDarkOrange, width: 0.25),
+                      borderRadius: BorderRadius.all(Radius.circular(16)),
+                    ),
+                    child: Container(
+                      width: 80,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Icon(
+                            Icons.category,
+                            color: BrolineColor.brolineDarkOrange,
+                          ),
+                          Text(
+                            "Category",
+                            style: TextStyle(
+                                color: BrolineColor.brolineDarkOrange,
+                                fontWeight: FontWeight.bold),
+                          )
+                        ],
+                      ),
                     ),
                   ),
                 );
@@ -127,7 +274,7 @@ class HomePage extends StatelessWidget {
       height: 350,
       decoration: BoxDecoration(
           color: BrolineColor.brolineLightGrey,
-          borderRadius: BorderRadius.circular(16)),
+          borderRadius: BorderRadius.circular(15)),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -145,8 +292,7 @@ class HomePage extends StatelessWidget {
               ),
               child: GridView.builder(
                   gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    childAspectRatio: 0.75,
-
+                      childAspectRatio: 0.75,
                       crossAxisCount: 2,
                       crossAxisSpacing: 2,
                       mainAxisSpacing: 2),
@@ -161,12 +307,17 @@ class HomePage extends StatelessWidget {
                           horizontal: 20,
                         ),
                         child: Column(
-                         // crossAxisAlignment: CrossAxisAlignment.start,
+                          // crossAxisAlignment: CrossAxisAlignment.start,
                           mainAxisAlignment: MainAxisAlignment.spaceAround,
                           children: [
                             Padding(
-                              padding: const EdgeInsets.symmetric(horizontal: 10,vertical: 5),
-                              child: Image.asset("assets/images/broline.jfif",fit: BoxFit.cover,height: 100,),
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 10, vertical: 5),
+                              child: Image.asset(
+                                "assets/images/broline.jfif",
+                                fit: BoxFit.cover,
+                                height: 100,
+                              ),
                             ),
                             Text(
                               "Omen Galaxy S10",
@@ -178,19 +329,35 @@ class HomePage extends StatelessWidget {
                               overflow: TextOverflow.ellipsis,
                             ),
                             ListTile(
-
-                              contentPadding: EdgeInsets.symmetric(horizontal: 0,vertical: 0),
+                              contentPadding: EdgeInsets.symmetric(
+                                  horizontal: 0, vertical: 0),
                               trailing: Container(
                                 width: 55,
-                                padding: EdgeInsets.only(left: 20,top: 5,bottom: 5),
+                                padding: EdgeInsets.only(
+                                    left: 20, top: 5, bottom: 5),
                                 decoration: BoxDecoration(
-                                  color: BrolineColor.brolineDarkOrange,
-                                  borderRadius: BorderRadius.only(topLeft: Radius.elliptical(50,35),bottomLeft: Radius.circular(100))
+                                    color: BrolineColor.brolineDarkOrange,
+                                    borderRadius: BorderRadius.only(
+                                        topLeft: Radius.elliptical(50, 35),
+                                        bottomLeft: Radius.circular(100))),
+                                child: Text(
+                                  "20% off",
+                                  maxLines: 2,
+                                  style: TextStyle(
+                                      color: BrolineColor.brolineWhite),
                                 ),
-                                child: Text("20% off",maxLines: 2,style: TextStyle(color: BrolineColor.brolineWhite),),
                               ),
-                              title: Text("\$300",style: TextStyle(color: BrolineColor.brolineDarkOrange,fontWeight: FontWeight.w700),),
-                              subtitle: Text("\$350",style: TextStyle(decoration: TextDecoration.lineThrough),),
+                              title: Text(
+                                "\$300",
+                                style: TextStyle(
+                                    color: BrolineColor.brolineDarkOrange,
+                                    fontWeight: FontWeight.w700),
+                              ),
+                              subtitle: Text(
+                                "\$350",
+                                style: TextStyle(
+                                    decoration: TextDecoration.lineThrough),
+                              ),
                             )
                           ],
                         ),
---
+++ lib/Pages/SubCategoryPage.dart
@@ -0,0 +1,242 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+import 'package:broline/Models/Colors.dart';
+
+class SubCategoryPage extends StatefulWidget {
+  final String categoryName;
+
+  SubCategoryPage({this.categoryName});
+
+  @override
+  _SubCategoryPageState createState() => _SubCategoryPageState();
+}
+
+class _SubCategoryPageState extends State<SubCategoryPage> {
+  bool isSterched;
+  Widget backArrow;
+  Widget search;
+  Widget title;
+  Widget _buildSearch(bool innerScrolled){
+    return IconButton(
+        icon: Icon(
+          Icons.search,
+          color: BrolineColor.brolineDarkGrey,
+        ),
+        onPressed: () {
+          setState(() {
+
+            if(isSterched == true){
+              title = Container(
+
+                  width: double.maxFinite,
+                  padding: EdgeInsets.symmetric(
+                      horizontal: 15, vertical: 3),
+                  decoration: BoxDecoration(
+                      color: BrolineColor.brolineLightGrey,
+                      borderRadius: BorderRadius.circular(16)),
+                  child: TextFormField(
+                      decoration: InputDecoration(
+                        icon: Icon(
+                          Icons.search,
+                          color: BrolineColor.brolineDarkGrey,
+                        ),
+                        hintText: 'Search Category',
+                        hintStyle: TextStyle(fontSize: 18),
+                      )));
+              isSterched = false;
+            }else{
+
+              title =   Text(
+                widget.categoryName,
+                style: TextStyle(color: BrolineColor.brolineDarkGrey),
+              );
+              isSterched = true;
+
+            }
+
+
+
+
+            print("Yes");
+
+
+          });
+        });
+  }
+
+  @override
+  void initState() {
+    isSterched = false;
+    backArrow =IconButton(
+        icon: Icon(
+          Icons.arrow_back_ios_outlined,
+          color: BrolineColor.brolineDarkGrey,
+        ),
+        onPressed: () {
+          setState(() {
+            if (isSterched == true) {
+              title = Text(
+                widget.categoryName,
+                style:
+                TextStyle(color: BrolineColor.brolineDarkGrey),
+              );
+              isSterched = false;
+            } else {
+              Navigator.of(context).pop();
+            }
+          });
+        });
+    search = _buildSearch(false);
+    title = Text(
+      widget.categoryName,
+      style: TextStyle(color: BrolineColor.brolineDarkGrey),
+    );
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: BrolineColor.brolineWhite,
+      body: NestedScrollView(
+        headerSliverBuilder: (BuildContext context, bool innerBoxScrolled) {
+          return <Widget>[
+            SliverPadding(
+              padding: EdgeInsets.only(top: 10, right: 10, left: 10),
+              sliver: SliverAppBar(
+                floating: true,
+                pinned: true,
+                title: innerBoxScrolled == true  ? title =Container(
+
+                    width: double.maxFinite,
+                    padding: EdgeInsets.symmetric(
+                        horizontal: 15, vertical: 3),
+                    decoration: BoxDecoration(
+                        color: BrolineColor.brolineLightGrey,
+                        borderRadius: BorderRadius.circular(16)),
+                    child: TextFormField(
+                        decoration: InputDecoration(
+                          icon: Icon(
+                            Icons.search,
+                            color: BrolineColor.brolineDarkGrey,
+                          ),
+                          hintText: 'Search Category',
+                          hintStyle: TextStyle(fontSize: 18),
+                        ))): isSterched == false ? Text(
+                  widget.categoryName,
+                  style: TextStyle(color: BrolineColor.brolineDarkGrey),
+                ):Container(
+
+                    width: double.maxFinite,
+                    padding: EdgeInsets.symmetric(
+                        horizontal: 15, vertical: 3),
+                    decoration: BoxDecoration(
+                        color: BrolineColor.brolineLightGrey,
+                        borderRadius: BorderRadius.circular(16)),
+                    child: TextFormField(
+                        decoration: InputDecoration(
+                          icon: Icon(
+                            Icons.search,
+                            color: BrolineColor.brolineDarkGrey,
+                          ),
+                          hintText: 'Search Category',
+                          hintStyle: TextStyle(fontSize: 18),
+                        ))),
+                elevation: 0.0,
+                actions: [
+                 innerBoxScrolled == true? SizedBox() : isSterched == true? SizedBox():_buildSearch(false)
+                ],
+                backgroundColor: BrolineColor.brolineWhite,
+                leading: IconButton(
+                    icon: Icon(
+                      Icons.arrow_back_ios_outlined,
+                      color: BrolineColor.brolineDarkGrey,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        if (isSterched == true) {
+                          title = Text(
+                            widget.categoryName,
+                            style:
+                            TextStyle(color: BrolineColor.brolineDarkGrey),
+                          );
+                          isSterched = false;
+                        } else {
+                          Navigator.of(context).pop();
+                        }
+                      });
+                    }),
+                ),
+              ),
+
+            // SliverPadding(
+            //     padding:
+            //         EdgeInsets.only(top: 10, right: 10, left: 10, bottom: 10),
+            //     sliver: SliverAppBar(
+            //       pinned: true,
+            //       floating: true,
+            //       elevation: 0.0,
+            //       backgroundColor: BrolineColor.brolineWhite,
+            //       leading: innerBoxScrolled == true ? backArrow : SizedBox() ,
+            //       title: Container(
+            //           width: double.maxFinite,
+            //           padding:
+            //               EdgeInsets.symmetric(horizontal: 15, vertical: 3),
+            //           decoration: BoxDecoration(
+            //               color: BrolineColor.brolineLightGrey,
+            //               borderRadius: BorderRadius.circular(16)),
+            //           child: TextFormField(
+            //               decoration: InputDecoration(
+            //             icon: Icon(
+            //               Icons.search,
+            //               color: BrolineColor.brolineDarkGrey,
+            //             ),
+            //             hintText: 'Search Category',
+            //             hintStyle: TextStyle(fontSize: 18),
+            //           ))),
+            //       stretch: true,
+            //       automaticallyImplyLeading: false,
+            //     )),
+          ];
+        },
+        body: CustomScrollView(
+          slivers: [
+            SliverList(
+              delegate: SliverChildBuilderDelegate(
+                (BuildContext context, int index) {
+                  return Container(
+                    height: 150,
+                    width: double.maxFinite,
+                    padding: EdgeInsets.symmetric(vertical: 20),
+                    child: Container(
+                      decoration: BoxDecoration(
+                          color: BrolineColor.brolineDarkOrange,
+                          borderRadius: BorderRadius.circular(15),
+                          image: DecorationImage(
+                            image: AssetImage('assets/images/laptop.jfif'),
+                            fit: BoxFit.cover,
+                          )),
+                      margin: EdgeInsets.symmetric(horizontal: 10),
+                      child: Padding(
+                        padding:
+                            EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+                        child: Text(
+                          "Electronics",
+                          style: TextStyle(
+                              color: BrolineColor.brolineWhite,
+                              fontSize: 30,
+                              fontWeight: FontWeight.w600),
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
