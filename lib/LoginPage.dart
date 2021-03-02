822d3bb4c502c726ad5939a0bcf70652bedc8324
BisratHailu <bisrathailu793@yahoo.com>
2021-03-03 01:14:52 +0300

    login page

--- lib/LoginPage.dart
+++ lib/LoginPage.dart
@@ -1,3 +1,5 @@
+import 'package:broline/Models/Colors.dart';
+import 'package:broline/Pages/Broline.dart';
 import 'package:broline/Providers/AuthProvider.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -16,73 +18,164 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Login'),
+        backgroundColor: BrolineColor.brolineDarkBlue,
+        title: Text('Broline'),
       ),
       body: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
-            RaisedButton(
-              child: Text('Login'),
-              onPressed: () {
-                final _auth = FirebaseAuth.instance;
-                print(_auth.currentUser);
-                _auth.verifyPhoneNumber(
-                    phoneNumber: '+16505553434',
-                    timeout: Duration(seconds: 60),
-                    verificationCompleted:
-                        (AuthCredential authCredential) async {
-                      try {
-                        UserCredential result =
-                            await _auth.signInWithCredential(authCredential);
-                        userRef.get().then((snapshot) {
-                          snapshot.docs.forEach((element) {
-                            if (result.additionalUserInfo.isNewUser) {
-                              CollectionReference addProfile = FirebaseFirestore
-                                  .instance
-                                  .collection("users");
-
-                              addProfile.add({
-                                'name': 'Titus',
-                                'phoneno': FirebaseAuth
-                                    .instance.currentUser.phoneNumber
-                              });
-                              Navigator.of(context).pushReplacement(
-                                  MaterialPageRoute(
-                                      builder: (context) => Profile()));
-                            }
-                            else {
-                              Navigator.of(context).pushReplacement(
-                                  MaterialPageRoute(
-                                      builder: (context) =>
-                                          Home(result.user.phoneNumber)));
-                            }
-                          });
-                        });
-
-                      } catch (e) {
-                        print(e);
-                      }
-                    },
-                    verificationFailed: (FirebaseAuthException authException) {
-                      print(authException.message);
-                    },
-                    codeSent: (String verificationId,
-                        [int forceResendingToken]) {
-                      //show dialog to take input from the user
-                      Navigator.of(context).pushReplacement(MaterialPageRoute(
-                          builder: (context) => Otp(verificationId)));
-                    },
-                    codeAutoRetrievalTimeout: (verificationId) {
-                      print(verificationId);
-                    });
-              },
+            Column(
+              children: [
+                Container(
+                  child: Text(
+                    "Welcome Back",
+                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.w500),
+                  ),
+                ),
+                Text("Login or Continue with Google")
+              ],
             ),
-            FlatButton(
-                onPressed: () {
-                  final auth = context.read<AuthProvider>();
-                  auth.Login();
-                },
-                child: Text('Login with google')),
+            // Container(
+            //   child: Image.asset(
+            //     "assets/images/brolineLogo.jpg",
+            //     fit: BoxFit.cover,
+            //     height: 100,
+            //     width: 60,
+            //   ),
+            // ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 50),
+              padding: EdgeInsets.symmetric(horizontal: 15, vertical: 3),
+              decoration: BoxDecoration(
+                  border: Border.all(color: BrolineColor.brolineDarkBlue),
+                  borderRadius: BorderRadius.circular(25)),
+              child: TextFormField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  focusedBorder: InputBorder.none,
+                  enabledBorder: InputBorder.none,
+                  errorBorder: InputBorder.none,
+                  disabledBorder: InputBorder.none,
+                  prefixIcon: Image.asset(
+                    "assets/images/flag.jfif",
+                    scale: 6,
+                  ),
+                  suffixIcon: Icon(
+                    Icons.phone_android,
+                    color: BrolineColor.brolineDarkBlue,
+                  ),
+                  hintText: "Enter your Phone number",
+                  prefixText: "+251",
+                ),
+              ),
+            ),
+            Column(
+              children: [
+                SizedBox(
+                  height: 45,
+                  width: MediaQuery.of(context).size.width - 100,
+                  child: RaisedButton(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20)),
+                    padding: EdgeInsets.symmetric(horizontal: 100),
+                    textColor: BrolineColor.brolineWhite,
+                    color: BrolineColor.brolineDarkBlue,
+                    child: Text(
+                      'Login',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    onPressed: () {
+                      final _auth = FirebaseAuth.instance;
+                      print(_auth.currentUser);
+                      _auth.verifyPhoneNumber(
+                          phoneNumber: '+16505553434',
+                          timeout: Duration(seconds: 60),
+                          verificationCompleted:
+                              (AuthCredential authCredential) async {
+                            try {
+                              UserCredential result = await _auth
+                                  .signInWithCredential(authCredential);
+                              userRef.get().then((snapshot) {
+                                snapshot.docs.forEach((element) {
+                                  if (result.additionalUserInfo.isNewUser) {
+                                    CollectionReference addProfile =
+                                        FirebaseFirestore.instance
+                                            .collection("users");
+
+                                    addProfile.add({
+                                      'name': 'Titus',
+                                      'phoneno': FirebaseAuth
+                                          .instance.currentUser.phoneNumber
+                                    });
+                                    Navigator.of(context).pushReplacement(
+                                        MaterialPageRoute(
+                                            builder: (context) => Profile()));
+                                  } else {
+                                    Navigator.of(context).pushReplacement(
+                                        MaterialPageRoute(
+                                            builder: (context) =>
+                                                Home(result.user.phoneNumber)));
+                                  }
+                                });
+                              });
+                            } catch (e) {
+                              print(e);
+                            }
+                          },
+                          verificationFailed:
+                              (FirebaseAuthException authException) {
+                            print(authException.message);
+                          },
+                          codeSent: (String verificationId,
+                              [int forceResendingToken]) {
+                            //show dialog to take input from the user
+                            Navigator.of(context).pushReplacement(
+                                MaterialPageRoute(
+                                    builder: (context) => Otp(verificationId)));
+                          },
+                          codeAutoRetrievalTimeout: (verificationId) {
+                            print(verificationId);
+                          });
+                    },
+                  ),
+                ),
+                SizedBox(
+                  height: 25,
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  margin: EdgeInsets.symmetric(horizontal: 50),
+                  child: FlatButton(
+                      padding: EdgeInsets.symmetric(horizontal: 50),
+                      height: 45,
+                      shape: RoundedRectangleBorder(
+                          side: BorderSide(color: BrolineColor.brolineDarkBlue),
+                          borderRadius: BorderRadius.circular(20)),
+                      onPressed: () {
+                        final auth = context.read<AuthProvider>();
+                        auth.Login();
+                      },
+                      child: Row(
+                        children: [
+                          Image.asset(
+                            "assets/images/google.png",
+                            fit: BoxFit.cover,
+                            height: 25,
+                          ),
+                          SizedBox(
+                            width: 20,
+                          ),
+                          Text(
+                            'Continue With Google',
+                            style: TextStyle(fontSize: 16),
+                          ),
+                        ],
+                      )),
+                ),
+              ],
+            ),
           ],
         ),
       ),
--- lib/Otp.dart
+++ lib/Otp.dart
@@ -1,63 +1,121 @@
+import 'package:broline/Models/Colors.dart';
+import 'package:broline/Pages/Broline.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:pin_code_fields/pin_code_fields.dart';
 
 import 'Home.dart';
 import 'Profile.dart';
 
 class Otp extends StatelessWidget {
   String verificationId;
+
   Otp(this.verificationId);
+
   @override
   Widget build(BuildContext context) {
     TextEditingController controller = TextEditingController();
     FirebaseAuth _auth = FirebaseAuth.instance;
-    CollectionReference userRef = FirebaseFirestore.instance.collection("users");
+    CollectionReference userRef =
+        FirebaseFirestore.instance.collection("users");
     return Scaffold(
       appBar: AppBar(
-        title: Text('OTP'),
+        backgroundColor: BrolineColor.brolineDarkBlue,
+        title: Text('OTP Verification'),
       ),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
-          Container(
-            child: TextField(
-              controller: controller,
-            ),
-
+          Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 50),
+                child: Text(
+                  "OTP Verification",
+                  maxLines: 2,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.w500),
+                ),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 50),
+                child: Text(
+                  "We have sent your code to +251*** the code will expire in 3 sec",
+                  maxLines: 2,
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ],
           ),
-          FlatButton(
-            child: Text("Done"),
-            textColor: Colors.white,
-            color: Colors.redAccent,
-            onPressed: () async {
+          Padding(
+              padding: EdgeInsets.symmetric(horizontal: 50),
+              child: PinCodeTextField(
+                onChanged: (value) {},
+                appContext: context,
+                pastedTextStyle: TextStyle(
+                  backgroundColor: BrolineColor.brolineWhite,
+                  color: BrolineColor.brolineBlack,
+                  fontWeight: FontWeight.bold,
+                ),
+                length: 6,
+                obscureText: true,
+                obscuringCharacter: '*',
+                blinkWhenObscuring: true,
+                animationType: AnimationType.fade,
+                pinTheme: PinTheme(
+                  activeColor: BrolineColor.brolineDarkBlue,
+                  inactiveColor: BrolineColor.brolineDarkBlue,
+                  shape: PinCodeFieldShape.underline,
+                  borderRadius: BorderRadius.circular(10),
+                  fieldHeight: 50,
+                  fieldWidth: 40,
+                ),
+                cursorColor: Colors.black,
+                animationDuration: Duration(milliseconds: 300),
+                backgroundColor: Colors.transparent,
+                controller: controller,
+                keyboardType: TextInputType.number,
+              )),
+          SizedBox(
+            height: 45,
+            width: MediaQuery.of(context).size.width - 100,
+            child: RaisedButton(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20)),
+              padding: EdgeInsets.symmetric(horizontal: 100),
+              textColor: BrolineColor.brolineWhite,
+              color: BrolineColor.brolineDarkBlue,
+              child: Text("Verify" , style: TextStyle(fontSize: 16),),
+              onPressed: () async {
+                AuthCredential _credential = PhoneAuthProvider.credential(
+                    verificationId: verificationId, smsCode: controller.text);
+                UserCredential result =
+                    await _auth.signInWithCredential(_credential);
+                userRef.get().then((snapshot) {
+                  snapshot.docs.forEach((element) {
+                    if (result.additionalUserInfo.isNewUser) {
+                      CollectionReference addProfile =
+                          FirebaseFirestore.instance.collection("users");
 
-              AuthCredential _credential = PhoneAuthProvider.credential(verificationId: verificationId, smsCode: controller.text);
-              UserCredential result = await _auth.signInWithCredential(_credential);
-              userRef.get().then((snapshot) {
-                snapshot.docs.forEach((element) {
-                  if (result.additionalUserInfo.isNewUser) {
-                    CollectionReference addProfile = FirebaseFirestore
-                        .instance
-                        .collection("users");
-
-                    addProfile.add({
-                      'name': 'Titus',
-                      'phoneno': FirebaseAuth
-                          .instance.currentUser.phoneNumber
-                    });
-                    Navigator.of(context).pushReplacement(
-                        MaterialPageRoute(
-                            builder: (context) => Profile()));
-                  }
-                  else {
-                    Navigator.of(context).pushReplacement(
-                        MaterialPageRoute(
-                            builder: (context) =>
-                                Home(result.user.phoneNumber)));
-                  }
+                      addProfile.add({
+                        'name': 'Titus',
+                        'phoneno': FirebaseAuth.instance.currentUser.phoneNumber
+                      });
+                      Navigator.of(context).pushReplacement(
+                          MaterialPageRoute(builder: (context) => Profile()));
+                    } else {
+                      Navigator.of(context).pushReplacement(MaterialPageRoute(
+                          builder: (context) => Broline()));
+                    }
+                  });
                 });
-              });
-            },
+              },
+            ),
           )
         ],
       ),
